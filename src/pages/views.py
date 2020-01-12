dc08b76156aec5afb5fe28448bbd257e8fd5e120
johnlemon <johnlemon@localhost.localdomain>
2020-01-12 16:38:03 +0100

    'try django 21'

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,13 +5,24 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
 	print(args, kwargs)
 	print(request.user)
-	return HttpResponse("<h1>Hello World</h1>")
+	#return HttpResponse("<h1>Hello World</h1>")
+	return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1>Contact</h1>")
+	#return HttpResponse("<h1>Contact</h1>")
+	return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1>About</h1>")
+	#return HttpResponse("<h1>About</h1>")
+	my_context = {
+		"title": "this is about us",
+		"this_is_true": True,
+		"my_number": 123,
+		"my_list": [123, 543, 321, "test"],
+		"my_html": "<h1>Hello World, from views.py</h1>"
+	
+	}
+	return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
 	return HttpResponse("<h1>Social</h1>")
